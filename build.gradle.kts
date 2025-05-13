01fd4527f64217c05c6ba05db5fbf93fd25ae1d6
Frederik Broer <Frederik.Broer@broercon.de>
2025-05-13 19:29:32 +0200

    Then annotate your class with @Entity, and the plugin will generate a no-arg constructor for

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("org.springframework.boot") version "3.2.5"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("plugin.spring") version "1.9.23"
+    kotlin("plugin.jpa") version "1.9.23"
 }
 
 group = "org.anaHome"
